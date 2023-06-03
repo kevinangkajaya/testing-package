5c8c79c09b4f567aebbb6f929398fbbea9b73657
kevinangkajaya <kevinangkajaya@yahoo.com>
2023-06-04 01:02:01 +0700

    test package from npmjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@kevinangkajaya/package-react-test';
+import Button from '@kevinangkajaya/package-react-test';
 
 function App() {
   return (
