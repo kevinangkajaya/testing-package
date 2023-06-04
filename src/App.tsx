336edd9b45a389753071b75e8bf342a4e1cacbfe
kevinangkajaya <kevinangkajaya@yahoo.com>
2023-06-04 13:11:36 +0700

    test peer dep (error-div)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@kevinangkajaya/package-react-test';
+import { ErrorDiv } from '@kevinangkajaya/error-div'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button label="Kaboom" />
+        <Button label="Kaboom" error="test error div 0.0.1" />
+        <ErrorDiv error="test error div 0.0.2" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
